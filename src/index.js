7bd1a6ed51f0abffef051f10373c96dc0428400c
BrennanTCarlyle <brennan.carlyle@gmail.com>
2020-02-19 00:37:30 -0900

    Added animations and hiding elements via button click

--- src/index.js
+++ src/index.js
@@ -6,9 +6,46 @@ var app = new Vue({
   data: {
     message: 'Hello Vue!',
     counter: 0,
-    userName: "BTCarlyle"
+    userName: "BTCarlyle",
+    mediumName: "Featured",
+        orderAmount: [],
+        mediumTypes: [
+          "Featured",
+          "Movies",
+          "Video Games",
+          "Historical",
+          "Comics",
+          "Other",
+          "Props",
+        ]
+      },
 
-  }
-});
+      el: '#popup',
+        data: {
+          show: true
+        },
+
+      computed: {
+        pluralMedium: function() {
+          if (this.mediumName == "Video Games") {
+            return "Cosplays for all of your favorite games!";
+          } else if (this.mediumName == "Featured") {
+            return this.mediumName;
+          } else {
+            return this.mediumName;
+          }
+        },
+        cannotPlaceOrder: function() {
+          return this.orderAmount < 1;
+        },
+        isDangerous: function() {
+          return this.orderAmount >= 10;
+        },
+        orderAmount: function() {
+          return this.mediumTypes.length;
+        }
+      }
+
+    });
 
 window.app = app;
