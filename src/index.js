abda887171437b87329766873afd382432e69b23
BrennanTCarlyle <brennan.carlyle@gmail.com>
2020-03-09 17:56:53 -0900

    added modal functionality

--- src/index.js
+++ src/index.js
@@ -1,60 +1,6 @@
 import Vue from 'vue/dist/vue.js'
 import "./style.css";
 
-
-
-// Open the Modal
-function openModal() {
-  document.getElementById("myModal").style.display = "block";
-}
-
-// Close the Modal
-function closeModal() {
-  document.getElementById("myModal").style.display = "none";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
-}
-
-
-
-// Get the <span> element that closes the modal
-//var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-//span.onclick = function() {
-  //modal.style.display = "none";
-//}
-
-
 var app = new Vue({
   el: '#app',
   data: {
@@ -63,25 +9,36 @@ var app = new Vue({
     currentPage: "Home",
     userName: "BTCarlyle",
     mediumName: null,
-        orderAmount: [],
-        mediumTypes: [
-          "Featured",
-          "Movies",
-          "Video Games",
-          "Historical",
-          "Comics",
-          "Other",
-          "Props",
-        ]
-      },
-
-
- methods: {
-    NewWindow: function() {
-            window.open("file:///C:/Users/Brennan/Documents/IM361/IM361REPO/im361web/Featured_shaxx.html");
-        }
-}
-
-    });
+    orderAmount: [],
+    mediumTypes: [
+      "Featured",
+      "Movies",
+      "Video Games",
+      "Historical",
+      "Comics",
+      "Other",
+      "Props",
+    ],
+    slideIndex: 1,
+    isModalShown: false,
+    modalImage: null,
+    modalImages: [
+      "./images/shaxxCosplay.jpg",
+      "./images/ornstein.jpg",
+      "./images/warhammer.jpg",
+      "./images/dredd.jpg",
+    ],
+  },
+  methods: {
+    openModal: function(image) {
+      this.modalImage = image;
+      this.isModalShown = true;
+    },
+    hideModal: function() {
+      this.modalImage = null;
+      this.isModalShown = false;
+    }
+  }
+});
 
 window.app = app;
